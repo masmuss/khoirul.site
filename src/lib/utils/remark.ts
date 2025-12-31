4d1c7c2a1a2dc52d711589e6bb79457fb2d2f1ba
Khoirul <ahmusafir.khoirul@gmail.com>
2025-12-31 19:41:56 +0700

    fix: resolve TypeScript errors in remark-github-card

--- src/lib/utils/remark.ts
+++ src/lib/utils/remark.ts
@@ -1,5 +1,5 @@
 import { h as _h, type Properties } from "hastscript";
-import type { Node, Paragraph as P, PhrasingContent } from "mdast";
+import type { Node, Paragraph as P, RootContent } from "mdast";
 import type { Directives } from "mdast-util-directive";
 
 /** Checks if a node is a directive. */
@@ -12,10 +12,10 @@ export function isNodeDirective(node: Node): node is Directives {
 }
 
 /** From Astro Starlight: Function that generates an mdast HTML tree ready for conversion to HTML by rehype. */
-export function h(el: string, attrs: Properties = {}, children: PhrasingContent[] = []): P {
+export function h(el: string, attrs: Properties = {}, children: RootContent[] = []): P {
 	const { properties, tagName } = _h(el, attrs);
 	return {
-		children,
+		children: children as P["children"],
 		data: { hName: tagName, hProperties: properties },
 		type: "paragraph",
 	};
