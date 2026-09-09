4eda9d22d009307e9cd4a6329719c5e6c927e562
Khoirul <ahmusafir.khoirul@gmail.com>
2026-09-09 19:29:45 +0700

    refactor(ui): remove unused UI components and packages

--- src/components/ui/tabs/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Tabs } from "./Tabs.astro";
-export {
-	default as TabsContent,
-	tabsContentVariants
-} from "./TabsContent.astro";
-export { default as TabsList, tabsListVariants } from "./TabsList.astro";
-export { default as TabsTrigger } from "./TabsTrigger.astro";
